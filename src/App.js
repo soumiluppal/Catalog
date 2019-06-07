ce5c949a29655cd3bfd2c798dc57e35ee042d83e
Soumil Uppal <soumil.barca@gmail.com>
2019-06-07 10:41:04 -0700

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
+
+import Home from './components/Home';
 
 import './styles/style.css';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        
+        <Home />
       </BrowserRouter>
     </div>
   );
